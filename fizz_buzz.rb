aca54331181d833d50614526006e60a08e2c3cdd
Ta-ryo <e165716@ie.u-ryukyu.ac.jp>
2019-07-11 21:17:59 +0900

    変数使を使わない条件分岐

--- fizz_buzz.rb
+++ fizz_buzz.rb
@@ -15,10 +15,9 @@ words = 1 + 2
 puts words
 puts 1 + 2
 
-num = gets.to_i
-if num >= 10
+if gets.to_i >= 10
   puts "10以上の数字です"
-  if num % 2 == 0
+  if gets.to_i % 2 == 0
     puts "また、偶数でもあります。"
   end
 else
