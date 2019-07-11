bb6a8bdfa9fbd47d944c514cfb983d878ba0f5a1
Ta-ryo <e165716@ie.u-ryukyu.ac.jp>
2019-07-11 22:05:46 +0900

    fizzbuzz

--- fizz_buzz.rb
+++ fizz_buzz.rb
@@ -1,21 +1,31 @@
-def fizz_buzz(n)
-  if n % 15 == 0
-    return 'FizzBuzz'
-  end
-  if n % 3 == 0
-    return 'Fizz'
-  elsif n % 5 == 0
-    return 'Buzz'
-  else
-    return n.to_s
+def fizz_buzz(num)
+  (1..num).each do |n|
+    if n % 15 == 0
+      puts 'FizzBuzz'
+    end
+    if n % 3 == 0
+      puts 'Fizz'
+    elsif n % 5 == 0
+      puts 'Buzz'
+    else
+      puts n.to_s
+    end
   end
 end
 
-sum = 0
-num = gets.to_i <= 0 ? 0
-(1..num).each do |nu|
-  if nu % 2 == 0
-    sum += nu
+n = gets.to_i
+fizz_buzz(n)
+
+input = gets.to_i
+
+def sum_up_to(last_number)
+  sum = 0
+  (1..last_number).each do |num|
+    if num % 2 == 0
+      sum += num
+    end
   end
+  return sum
 end
-puts sum
+
+puts sum_up_to(input)
