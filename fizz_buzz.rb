8d7fe3c5cb5292e1587dfa1216f100ec783600c2
Ta-ryo <e165716@ie.u-ryukyu.ac.jp>
2019-07-11 21:25:29 +0900

    roup文を書いた

--- fizz_buzz.rb
+++ fizz_buzz.rb
@@ -11,15 +11,9 @@ def fizz_buzz(n)
   end
 end
 
-words = 1 + 2
-puts words
-puts 1 + 2
-
-if gets.to_i >= 10
-  puts "10以上の数字です"
-  if gets.to_i % 2 == 0
-    puts "また、偶数でもあります。"
-  end
-else
-  puts "10未満の数字です"
+sum = 0
+(1..10).each do |num|
+  puts sum
+  sum += num
 end
+puts sum
